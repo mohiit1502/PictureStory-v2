fa6911766c63102d5d892124b533ac3bf05cc8c8
vaasu <mohiit1502@gmail.com>
2020-03-07 05:16:28 +0530

    Updates

---
+++ precache-manifest.7b184157c6f1b720baf34bd861670d23.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71cdb90f46104fb158abaafac2303bbf",
+    "url": "/PictureStory-v2/index.html"
+  },
+  {
+    "revision": "f37843c47578a402295c",
+    "url": "/PictureStory-v2/static/css/3.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "266edeed8c90785f0986",
+    "url": "/PictureStory-v2/static/css/6.e9cfa895.chunk.css"
+  },
+  {
+    "revision": "eac36aca24deb69c6390",
+    "url": "/PictureStory-v2/static/css/main.7457aa92.chunk.css"
+  },
+  {
+    "revision": "7a82c07c68b430a45d0b",
+    "url": "/PictureStory-v2/static/js/0.c8ec65f8.chunk.js"
+  },
+  {
+    "revision": "f37843c47578a402295c",
+    "url": "/PictureStory-v2/static/js/3.1b70b419.chunk.js"
+  },
+  {
+    "revision": "d11fba877f1aa15e15da",
+    "url": "/PictureStory-v2/static/js/4.2660496a.chunk.js"
+  },
+  {
+    "revision": "3d6fdec8c6658bd2c0f8",
+    "url": "/PictureStory-v2/static/js/5.45d2a28a.chunk.js"
+  },
+  {
+    "revision": "266edeed8c90785f0986",
+    "url": "/PictureStory-v2/static/js/6.5d89ab0e.chunk.js"
+  },
+  {
+    "revision": "64bf9b36eaaa4f8de4d6",
+    "url": "/PictureStory-v2/static/js/7.5392c4be.chunk.js"
+  },
+  {
+    "revision": "3361a52a7ea8914a8e7b",
+    "url": "/PictureStory-v2/static/js/8.8e6e1df7.chunk.js"
+  },
+  {
+    "revision": "6a6e44583331aa6ad796",
+    "url": "/PictureStory-v2/static/js/9.1031cee6.chunk.js"
+  },
+  {
+    "revision": "eac36aca24deb69c6390",
+    "url": "/PictureStory-v2/static/js/main.bfe56b60.chunk.js"
+  },
+  {
+    "revision": "5db277adf5f4437ffc6c",
+    "url": "/PictureStory-v2/static/js/runtime-main.b63b237d.js"
+  }
+]);
